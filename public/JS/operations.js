22826bdeca112ae75877408b3b20dcf6fc153d66
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-03-14 07:46:51 -0600

    create en public folders -> images, CSS y JS

---
+++ public/JS/operations.js
@@ -0,0 +1,41 @@
+// tableOperations.js
+
+export function insertarThead() {
+  return new Promise((resolve, reject) => {
+    const primeraFila = document.querySelector('table tbody tr:nth-child(1)');
+
+    // html thead
+    const thead = `
+  <thead>
+    <tr>${primeraFila.innerHTML}</tr>
+  </thead>
+  `;
+
+    const table = document.querySelector('#tablePreview > table');
+    table.insertAdjacentHTML('afterbegin', thead);
+
+    // Borrar el elemento primera fila
+    primeraFila.remove();
+    resolve();
+  });
+}
+
+export function mostrarNombreArchivo(fileInput) {
+  const nombreArchivo = document.getElementById('nombreArchivo');
+  const nombreArchivoElement = document.querySelector('.nombre-de-archivo');
+
+  if (!nombreArchivo || !nombreArchivoElement) return;
+
+  if (fileInput.files.length > 0) {
+    const nombre = fileInput.files[0].name;
+
+    nombreArchivo.innerHTML = `${nombre}`;
+    nombreArchivoElement.style.opacity = '1';
+    nombreArchivoElement.classList.remove('animarTexto');
+    setTimeout(() => {
+      nombreArchivoElement.classList.add('animarTexto');
+    }, 50);
+  } else {
+    nombreArchivo.innerHTML = '';
+  }
+}
