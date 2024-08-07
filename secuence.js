af4743bdb9d3dfe0c707c67331a08597889552f0
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-06 23:30:30 -0600

    feat:Inicio de secuence page

---
+++ secuence.js
@@ -0,0 +1,40 @@
+console.log('Secuence.js');
+
+import { navbar, parametrosDeLaUrl, eventoClickCheckBox } from './public/JS/funcionesGlobales.js';
+
+import {
+  handleFileInputChange,
+  handleDragOver,
+  handleDragLeave,
+  handleDrop,
+} from './public/JS/fileUpload.js';
+
+async function content() {
+  console.log('Content');
+  document.getElementById('preloader').style.display = 'none';
+  try {
+    const container = document.querySelector('.container-file-upload-form');
+    const fileInput = document.getElementById('fileInput');
+
+    if (!fileInput) return;
+
+    // Agregar evento de cambio para la carga de archivos
+    fileInput.addEventListener('change', e => handleFileInputChange(e, fileInput, 'SECUENCE'));
+
+    // Agregar eventos para arrastrar y soltar
+    container.addEventListener('dragover', e => handleDragOver(e, container));
+    container.addEventListener('dragleave', e => handleDragLeave(e, container));
+    container.addEventListener('drop', e => handleDrop(e, container, fileInput, 'SECUENCE'));
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+
+  parametrosDeLaUrl();
+  navbar();
+
+  eventoClickCheckBox()
+    .then(msg => console.log(msg))
+    .catch(err => console.error('Error al crear el evento click mostrar:', err));
+}
+
+window.addEventListener('DOMContentLoaded', content);
