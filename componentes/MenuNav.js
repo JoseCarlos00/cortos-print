3b4b6bf621c763e4118b94298c851c65fdbdd5e7
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-10-02 11:15:45 -0600

    feat: se crearon conponentes para Nav, ButtonUp y FileUpload

---
+++ componentes/MenuNav.js
@@ -0,0 +1,55 @@
+export default class MenuNav extends HTMLElement {
+  static nameElement = 'menu-nav'
+
+  static get observedAttributes() {
+    return ['active'];
+  }
+
+
+  constructor() {
+    super()
+
+    this.active = '';
+    this.hrefs = [
+      { text: 'Cortos', href: './index.html?ordenar=BODEGA', },
+      { text: 'Trabajos Activos', href: './trabajosActivos.html?ordenar=SHIP_TO' },
+      { text: 'Inventory', href: './inventory.html?ordenar=NoOrdenar' },
+      { text: 'Secuencia', href: './secuence.html?ordenar=NoOrdenar' },
+    ]
+  }
+
+  createUlElement() {
+    const ulElement = document.createElement('ul');
+
+    this.hrefs.forEach(({ text, href }) => {
+      const li = document.createElement('li');
+      li.className = 'nav-item';
+
+      if (this.active === text) {
+        li.innerHTML = `<a class="nav-link active" aria-current="page" href="#">${text}</a>`
+      } else {
+        li.innerHTML = `<a class="nav-link" href="${href}">${text}</a>`
+      }
+
+      ulElement.appendChild(li);
+    });
+
+    return ulElement;
+  }
+
+  connectedCallback() {
+    this.innerHTML = /*html*/`
+      <nav class="navbar navbar-expand-lg bg-primary">
+      <div class="container-fluid justify-content-center">
+        <ul class="nav nav-underline">
+          ${this.createUlElement().innerHTML}
+        </ul>
+      </div>
+    </nav>
+    `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.active = newValue;
+  }
+}
---
+++ componentes/componentes.js
@@ -0,0 +1,7 @@
+import MenuNav from './MenuNav.js';
+import FileUpload from './FileUpload.js';
+import ButtonUp from './ButtonUp.js'
+
+customElements.define(MenuNav.nameElement, MenuNav)
+customElements.define(FileUpload.nameElement, FileUpload)
+customElements.define(ButtonUp.nameElement, ButtonUp)
