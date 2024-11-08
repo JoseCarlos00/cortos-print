7585e2c2464f6aa7bd89c46ca1207a038c7705e1
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-11-08 16:09:45 -0600

    Refactor: Ordenar por piso se agrego una condicion para verificar si alguna colunana contiene la palabra bodega  y redirecionar al metodo correcto

--- public/JS/sortTable.js
+++ public/JS/sortTable.js
@@ -1,157 +1,194 @@
 export function sortValueNumeric(rows, table, headerPositionElement) {
-  return new Promise((resolve, reject) => {
-    try {
-      if (!rows || !table || !headerPositionElement) {
-        return reject('Error al ordenar tabla');
-      }
+	return new Promise((resolve, reject) => {
+		try {
+			if (!rows || !table || !headerPositionElement) {
+				return reject("Error al ordenar tabla");
+			}
 
-      rows.sort((a, b) => {
-        // Utilizar el valor de la URL en el selector
-        let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
-        let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
-        aValue = aValue.split('-').pop();
-        bValue = bValue.split('-').pop();
-        // Verificar si los valores son numéricos
-        const aValueNumeric = !isNaN(parseFloat(aValue)) && isFinite(aValue);
-        const bValueNumeric = !isNaN(parseFloat(bValue)) && isFinite(bValue);
-        // Comparar los valores numéricos
-        if (aValueNumeric && bValueNumeric) {
-          return parseFloat(aValue) - parseFloat(bValue);
-        } else {
-          // Si al menos uno de los valores no es numérico, comparar como cadenas
-          return aValue.localeCompare(bValue);
-        }
-      });
+			rows.sort((a, b) => {
+				// Utilizar el valor de la URL en el selector
+				let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
+				let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
+				aValue = aValue.split("-").pop();
+				bValue = bValue.split("-").pop();
+				// Verificar si los valores son numéricos
+				const aValueNumeric = !isNaN(parseFloat(aValue)) && isFinite(aValue);
+				const bValueNumeric = !isNaN(parseFloat(bValue)) && isFinite(bValue);
+				// Comparar los valores numéricos
+				if (aValueNumeric && bValueNumeric) {
+					return parseFloat(aValue) - parseFloat(bValue);
+				} else {
+					// Si al menos uno de los valores no es numérico, comparar como cadenas
+					return aValue.localeCompare(bValue);
+				}
+			});
 
-      // Reinsertar las filas ordenadas en la tabla
-      rows.forEach(row => {
-        table.querySelector('tbody').appendChild(row);
-      });
+			// Reinsertar las filas ordenadas en la tabla
+			rows.forEach((row) => {
+				table.querySelector("tbody").appendChild(row);
+			});
 
-      resolve('Ordenar tabla por valor numerico correcto');
-    } catch (error) {
-      reject(`Error al ordenar la tabla: ${error}`);
-    }
-  });
+			resolve("Ordenar tabla por valor numerico correcto");
+		} catch (error) {
+			reject(`Error al ordenar la tabla: ${error}`);
+		}
+	});
 }
 
 export function sortValueString(rows, table, headerPositionElement) {
-  return new Promise((resolve, reject) => {
-    try {
-      if (!rows || !table || !headerPositionElement) {
-        return reject('Error al ordenar tabla');
-      }
+	return new Promise((resolve, reject) => {
+		try {
+			if (!rows || !table || !headerPositionElement) {
+				return reject("Error al ordenar tabla");
+			}
 
-      rows.sort((a, b) => {
-        // Utilizar el valor de la URL en el selector
-        let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
-        let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
-        return aValue.localeCompare(bValue);
-      });
+			rows.sort((a, b) => {
+				// Utilizar el valor de la URL en el selector
+				let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
+				let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
+				return aValue.localeCompare(bValue);
+			});
 
-      // Reinsertar las filas ordenadas en la tabla
-      rows.forEach(row => {
-        table.querySelector('tbody').appendChild(row);
-      });
+			// Reinsertar las filas ordenadas en la tabla
+			rows.forEach((row) => {
+				table.querySelector("tbody").appendChild(row);
+			});
 
-      resolve('Ordenar tabla por cadena de texto correcto');
-    } catch (error) {
-      reject(`Error al ordenar la tabla: ${error}`);
-    }
-  });
+			resolve("Ordenar tabla por cadena de texto correcto");
+		} catch (error) {
+			reject(`Error al ordenar la tabla: ${error}`);
+		}
+	});
 }
 
 export function ordenarPorBodega(rows, table, headerPositionElement) {
-  return new Promise((resolve, reject) => {
-    try {
-      rows.sort((a, b) => {
-        let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
-        let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
+	return new Promise((resolve, reject) => {
+		try {
+			rows.sort((a, b) => {
+				let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
+				let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
 
-        // Eliminar 'W-Mar Bodega ' de los valores si está presente
-        if (aValue.startsWith('W-Mar Bodega ')) {
-          aValue = aValue.replace('W-Mar Bodega ', '');
-        }
-        if (bValue.startsWith('W-Mar Bodega ')) {
-          bValue = bValue.replace('W-Mar Bodega ', '');
-        }
+				// Eliminar 'W-Mar Bodega ' de los valores si está presente
+				if (aValue.startsWith("W-Mar Bodega ")) {
+					aValue = aValue.replace("W-Mar Bodega ", "");
+				}
+				if (bValue.startsWith("W-Mar Bodega ")) {
+					bValue = bValue.replace("W-Mar Bodega ", "");
+				}
 
-        // Verificar si los valores son numéricos
-        const aValueNumeric = !isNaN(parseFloat(aValue)) && isFinite(aValue);
-        const bValueNumeric = !isNaN(parseFloat(bValue)) && isFinite(bValue);
+				// Verificar si los valores son numéricos
+				const aValueNumeric = !isNaN(parseFloat(aValue)) && isFinite(aValue);
+				const bValueNumeric = !isNaN(parseFloat(bValue)) && isFinite(bValue);
 
-        // Comparar los valores numéricos
-        if (aValueNumeric && bValueNumeric) {
-          return parseFloat(aValue) - parseFloat(bValue);
-        } else {
-          // Si al menos uno de los valores no es numérico, comparar como cadenas
-          return aValue.localeCompare(bValue);
-        }
-      });
+				// Comparar los valores numéricos
+				if (aValueNumeric && bValueNumeric) {
+					return parseFloat(aValue) - parseFloat(bValue);
+				} else {
+					// Si al menos uno de los valores no es numérico, comparar como cadenas
+					return aValue.localeCompare(bValue);
+				}
+			});
 
-      // Reinsertar las filas ordenadas en la tabla
-      const tbody = table.querySelector('tbody');
-      rows.forEach(row => {
-        tbody?.appendChild(row);
-      });
+			// Reinsertar las filas ordenadas en la tabla
+			const tbody = table.querySelector("tbody");
+			rows.forEach((row) => {
+				tbody?.appendChild(row);
+			});
 
-      resolve('Ordenar tabla por valor bodega correcto');
-    } catch (error) {
-      reject(`Error al ordenar la tabla: ${error}`);
-    }
-  });
+			resolve("Ordenar tabla por valor bodega correcto");
+		} catch (error) {
+			reject(`Error al ordenar la tabla: ${error}`);
+		}
+	});
+}
+
+export function ordenarPorPiso2(rows, table, headerPositionElement) {
+	const primerPiso = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "W-Mar Vinil", "W-Mar Mayoreo"];
+	const segundoPiso = ["10", "11", "12", "13", "14", "15", "16", "17", "W-Mar No Banda"];
+
+	const getIndex = (value) => {
+		if (primerPiso.includes(value)) {
+			return { index: primerPiso.indexOf(value), priority: 1 };
+		} else if (segundoPiso.includes(value)) {
+			return { index: segundoPiso.indexOf(value), priority: 2 };
+		} else {
+			return { index: Infinity, priority: Infinity };
+		}
+	};
+
+	return new Promise((resolve, reject) => {
+		try {
+			// Ordenar las filas
+			rows.sort((a, b) => {
+				let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
+				let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
+
+				// Eliminar 'W-Mar Bodega ' de los valores si está presente
+				if (aValue.startsWith("W-Mar Bodega ")) {
+					aValue = aValue.replace("W-Mar Bodega ", "");
+				}
+				if (bValue.startsWith("W-Mar Bodega ")) {
+					bValue = bValue.replace("W-Mar Bodega ", "");
+				}
+
+				// Obtener los índices de los valores en los arrays de prioridad
+				const aIndex = getIndex(aValue);
+				const bIndex = getIndex(bValue);
+
+				// Comparar primero por prioridad y luego por índice
+				if (aIndex.priority !== bIndex.priority) {
+					return aIndex.priority - bIndex.priority;
+				} else {
+					return aIndex.index - bIndex.index;
+				}
+			});
+
+			// Reinsertar las filas ordenadas en la tabla
+			const tbody = table.querySelector("tbody");
+			rows.forEach((row) => {
+				tbody.appendChild(row);
+			});
+
+			resolve("Ordenar tabla por valor bodega correcto");
+		} catch (error) {
+			reject(`Error al ordenar la tabla: ${error.message}`);
+		}
+	});
 }
 
 export function ordenarPorPiso(rows, table, headerPositionElement) {
-  const primerPiso = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'W-Mar Vinil', 'W-Mar Mayoreo'];
-  const segundoPiso = ['10', '11', '12', '13', '14', '15', '16', '17', 'W-Mar No Banda'];
+	return new Promise((resolve, reject) => {
+		try {
+			if (!rows || !table || !headerPositionElement) {
+				return reject("Error al ordenar tabla");
+			}
 
-  const getIndex = value => {
-    if (primerPiso.includes(value)) {
-      return { index: primerPiso.indexOf(value), priority: 1 };
-    } else if (segundoPiso.includes(value)) {
-      return { index: segundoPiso.indexOf(value), priority: 2 };
-    } else {
-      return { index: Infinity, priority: Infinity };
-    }
-  };
+			const hasWarehouseSomeRow = rows.some((row) =>
+				row.querySelector(`td:nth-child(${headerPositionElement})`)?.innerText?.includes("W-Mar Bodega")
+			);
 
-  return new Promise((resolve, reject) => {
-    try {
-      // Ordenar las filas
-      rows.sort((a, b) => {
-        let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
-        let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText.trim();
+			if (hasWarehouseSomeRow) {
+				ordenarPorPiso2(rows, table, headerPositionElement);
+				return;
+			}
 
-        // Eliminar 'W-Mar Bodega ' de los valores si está presente
-        if (aValue.startsWith('W-Mar Bodega ')) {
-          aValue = aValue.replace('W-Mar Bodega ', '');
-        }
-        if (bValue.startsWith('W-Mar Bodega ')) {
-          bValue = bValue.replace('W-Mar Bodega ', '');
-        }
+			rows.sort((a, b) => {
+				// Utilizar el valor de la URL en el selector
+				let aValue = a.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
+				let bValue = b.querySelector(`td:nth-child(${headerPositionElement})`).innerText;
 
-        // Obtener los índices de los valores en los arrays de prioridad
-        const aIndex = getIndex(aValue);
-        const bIndex = getIndex(bValue);
+				// Comparar los valores como cadenas de texto
+				return aValue.localeCompare(bValue, undefined, { numeric: true });
+			});
 
-        // Comparar primero por prioridad y luego por índice
-        if (aIndex.priority !== bIndex.priority) {
-          return aIndex.priority - bIndex.priority;
-        } else {
-          return aIndex.index - bIndex.index;
-        }
-      });
+			// Reinsertar las filas ordenadas en la tabla
+			rows.forEach((row) => {
+				table.querySelector("tbody").appendChild(row);
+			});
 
-      // Reinsertar las filas ordenadas en la tabla
-      const tbody = table.querySelector('tbody');
-      rows.forEach(row => {
-        tbody.appendChild(row);
-      });
-
-      resolve('Ordenar tabla por valor bodega correcto');
-    } catch (error) {
-      reject(`Error al ordenar la tabla: ${error.message}`);
-    }
-  });
+			resolve("Ordenar tabla por cadena de texto correcto");
+		} catch (error) {
+			reject(`Error al ordenar la tabla: ${error}`);
+		}
+	});
 }
--- public/fileProcessing/cortos/ordenarPorPiso.js
+++ public/fileProcessing/cortos/ordenarPorPiso.js
@@ -1,77 +1,84 @@
+import { insertarPageBreak } from "../../JS/funcionesGlobales.js ";
+
 const primerPiso = [
-  'W-Mar Bodega 1',
-  'W-Mar Bodega 2',
-  'W-Mar Bodega 3',
-  'W-Mar Bodega 4',
-  'W-Mar Bodega 5',
-  'W-Mar Bodega 6',
-  'W-Mar Bodega 7',
-  'W-Mar Bodega 8',
-  'W-Mar Bodega 9',
-  'W-Mar Vinil',
-  'W-Mar Mayoreo',
+	"W-Mar Bodega 1",
+	"W-Mar Bodega 2",
+	"W-Mar Bodega 3",
+	"W-Mar Bodega 4",
+	"W-Mar Bodega 5",
+	"W-Mar Bodega 6",
+	"W-Mar Bodega 7",
+	"W-Mar Bodega 8",
+	"W-Mar Bodega 9",
+	"W-Mar Vinil",
+	"W-Mar Mayoreo",
 ];
 const segundoPiso = [
-  'W-Mar Bodega 10',
-  'W-Mar Bodega 11',
-  'W-Mar Bodega 12',
-  'W-Mar Bodega 13',
-  'W-Mar Bodega 14',
-  'W-Mar Bodega 15',
-  'W-Mar Bodega 16',
-  'W-Mar Bodega 17',
-  'W-Mar No Banda',
+	"W-Mar Bodega 10",
+	"W-Mar Bodega 11",
+	"W-Mar Bodega 12",
+	"W-Mar Bodega 13",
+	"W-Mar Bodega 14",
+	"W-Mar Bodega 15",
+	"W-Mar Bodega 16",
+	"W-Mar Bodega 17",
+	"W-Mar No Banda",
 ];
 
 export function insertarPageBreakPorPiso(positionElement) {
-  return new Promise((resolve, reject) => {
-    // Busca y agregar la clase page-break para el salto de página por el valor de la URL
-    const table = document.querySelector('#tablePreview table');
+	return new Promise((resolve, reject) => {
+		// Busca y agregar la clase page-break para el salto de página por el valor de la URL
+		const table = document.querySelector("#tablePreview table");
 
-    if (!table) {
-      return reject('No se encontró la tabla con el id: #tablePreview');
-    }
+		if (!table) {
+			return reject("No se encontró la tabla con el id: #tablePreview");
+		}
 
-    if (!positionElement) {
-      return reject('No se encontró la posición del elemento');
-    }
+		if (!positionElement) {
+			return reject("No se encontró la posición del elemento");
+		}
 
-    // Filtrar y agregar clase al primer TD de cada grupo
-    const filas = table.querySelectorAll('tr');
+		// Filtrar y agregar clase al primer TD de cada grupo
+		const filas = Array.from(table.querySelectorAll("tr"));
 
-    // Función para determinar el grupo (primer piso o segundo piso)
-    const getGroup = value => {
-      if (primerPiso.includes(value)) {
-        return 'primerPiso';
-      } else if (segundoPiso.includes(value)) {
-        return 'segundoPiso';
-      } else {
-        return 'otro';
-      }
-    };
+		const hasWarehouseSomeRow = filas.some((row) =>
+			row.querySelector(`td:nth-child(${positionElement})`)?.innerText?.includes("W-Mar Bodega")
+		);
 
-    // Iterar sobre las filas
-    filas.forEach((fila, index) => {
-      // Ignorar la primera fila (encabezados)
-      if (index === 0) return;
+		if (hasWarehouseSomeRow) {
+			insertarPageBreak(positionElement);
+			return;
+		}
 
-      const valorDeLaFilaActual = fila
-        .querySelector(`td:nth-child(${positionElement})`)
-        .textContent.trim();
-      const valorDeLaFilaAnterior =
-        index > 1
-          ? filas[index - 1].querySelector(`td:nth-child(${positionElement})`).textContent.trim()
-          : null;
+		// Función para determinar el grupo (primer piso o segundo piso)
+		const getGroup = (value) => {
+			if (primerPiso.includes(value)) {
+				return "primerPiso";
+			} else if (segundoPiso.includes(value)) {
+				return "segundoPiso";
+			} else {
+				return "otro";
+			}
+		};
 
-      const grupoActual = getGroup(valorDeLaFilaActual);
-      const grupoAnterior = valorDeLaFilaAnterior ? getGroup(valorDeLaFilaAnterior) : null;
+		// Iterar sobre las filas
+		filas.forEach((fila, index) => {
+			// Ignorar la primera fila (encabezados)
+			if (index === 0) return;
 
-      // Verificar si el grupo actual es diferente al grupo anterior
-      if (grupoActual !== grupoAnterior && grupoAnterior) {
-        filas[index - 1].classList.add('page-break');
-      }
-    });
+			const valorDeLaFilaActual = fila.querySelector(`td:nth-child(${positionElement})`).textContent.trim();
+			const valorDeLaFilaAnterior =
+				index > 1 ? filas[index - 1].querySelector(`td:nth-child(${positionElement})`).textContent.trim() : null;
 
-    resolve('Insertar PageBreak con éxito');
-  });
+			const grupoActual = getGroup(valorDeLaFilaActual);
+			const grupoAnterior = valorDeLaFilaAnterior ? getGroup(valorDeLaFilaAnterior) : null;
+
+			// Verificar si el grupo actual es diferente al grupo anterior
+			if (grupoActual !== grupoAnterior && grupoAnterior) {
+				filas[index - 1].classList.add("page-break");
+			}
+		});
+
+		resolve("Insertar PageBreak con éxito");
+	});
 }
