a29c1ea408e9a6994f28364d8c755e7644995bae
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-12-03 15:12:09 -0600

    Feat: Se agrego en la creacion del  thead un indice y un elemento resizer

--- public/JS/operations.js
+++ public/JS/operations.js
@@ -1,58 +1,68 @@
 // tableOperations.js
 
 export function insertarThead() {
-  return new Promise((resolve, reject) => {
-    const primeraFila = document.querySelector('table tbody tr:nth-child(1)');
+	return new Promise((resolve, reject) => {
+		// html thead
 
-    if (!primeraFila) {
-      return reject('No se encontro la primera fila el [thead]');
-    }
+		const table = document.querySelector("#tablePreview > table");
 
-    // html thead
-    const thead = `
-  <thead>
-    <tr>${primeraFila.innerHTML}</tr>
-  </thead>
-  `;
+		if (!table) {
+			return reject("No se encontro la tabla con el id: #tablePreview table");
+		}
 
-    const table = document.querySelector('#tablePreview > table');
+		const caption = document.querySelector("#myCaption");
 
-    if (!table) {
-      return reject('No se encontro la tabla con el id: #tablePreview table');
-    }
+		const firstRow = table.rows[0];
+		if (!firstRow) {
+			throw new Error("[FileManager]:[insertTagThead]: 'No rows found'");
+		}
 
-    const caption = document.querySelector('#myCaption');
+		const rowsHeaders = Array.from(firstRow.cells);
+		if (rowsHeaders.length === 0) {
+			throw new Error("[FileManager]:[insertTagThead]: 'No cells found'");
+		}
 
-    if (caption) {
-      caption.insertAdjacentHTML('afterend', thead);
-    } else {
-      table.insertAdjacentHTML('afterbegin', thead);
-    }
+		const thead = table.createTHead();
+		const row = thead.insertRow(0);
 
-    // Borrar el elemento primera fila
-    primeraFila.remove();
-    resolve();
-  });
+		rowsHeaders.forEach((td, index) => {
+			const th = document.createElement("td");
+			th.innerHTML = td.textContent + `<div class="resizer">`;
+			th.dataset.colIndex = index + 1;
+
+			row.appendChild(th);
+		});
+
+		firstRow.remove();
+
+		if (caption) {
+			caption.insertAdjacentElement("afterend", thead);
+		} else {
+			table.insertAdjacentElement("afterbegin", thead);
+		}
+
+		resolve();
+	});
 }
 
 export function mostrarNombreArchivo(fileInput) {
-  const nombreArchivo = document.getElementById('nombreArchivo');
-  const nombreArchivoElement = document.querySelector('.nombre-de-archivo');
+	const nombreArchivo = document.getElementById("nombreArchivo");
+	const nombreArchivoElement = document.querySelector(".nombre-de-archivo");
 
-  if (!nombreArchivo || !nombreArchivoElement) return;
+	if (!nombreArchivo || !nombreArchivoElement) return;
 
-  if (fileInput.files.length > 0) {
-    const nombre = fileInput.files[0].name;
+	if (fileInput.files.length > 0) {
+		const nombre = fileInput.files[0].name;
 
-    nombreArchivo.innerHTML = `${nombre}`;
-    nombreArchivoElement.style.opacity = '1';
-    nombreArchivoElement.classList.remove('animarTexto');
-    setTimeout(() => {
-      nombreArchivoElement.classList.add('animarTexto');
-    }, 50);
-  } else {
-    nombreArchivo.innerHTML = '';
-  }
+		nombreArchivo.innerHTML = `${nombre}`;
+		nombreArchivoElement.style.opacity = "1";
+		nombreArchivoElement.classList.remove("animarTexto");
+		setTimeout(() => {
+			nombreArchivoElement.classList.add("animarTexto");
+		}, 50);
+	} else {
+		nombreArchivo.innerHTML = "";
+	}
 }
 
 /**
@@ -62,17 +72,55 @@ export function mostrarNombreArchivo(fileInput) {
  * @returns Posicion del elemento buscado en el Array [indice]
  */
 export function getHeaderPosition(headerRow, values) {
-  console.log('[Get Header Position]', headerRow.length);
+	console.log("[Get Header Position]", headerRow.length);
 
-  for (let i = 0; i < headerRow.length; i++) {
-    const header = headerRow[i].innerHTML.toLowerCase().trim();
-    for (let j = 0; j < values.length; j++) {
-      const value = values[j].toLowerCase().trim();
-      if (header === value) {
-        const positionElement = i + 1;
-        return positionElement;
-      }
-    }
-  }
-  return null; // Si no se encuentra ninguna coincidencia
+	for (let i = 0; i < headerRow.length; i++) {
+		const header = headerRow[i].innerHTML.toLowerCase().trim();
+		for (let j = 0; j < values.length; j++) {
+			const value = values[j].toLowerCase().trim();
+			if (header === value) {
+				const positionElement = i + 1;
+				return positionElement;
+			}
+		}
+	}
+	return null; // Si no se encuentra ninguna coincidencia
+}
+
+export function setRezizeEvent() {
+	const resizers = document.querySelectorAll(".resizer");
+	const table = document.querySelector("table");
+
+	resizers.forEach((resizer) => {
+		resizer.addEventListener("mousedown", function (e) {
+			const th = e.target.parentNode;
+
+			console.log(th);
+
+			const { colIndex } = th.dataset;
+			console.log("Col Index:", colIndex);
+
+			if (colIndex < 0 || !colIndex || colIndex > table.rows[0].cells.length) {
+				console.warn("Col Index invalido");
+				return;
+			}
+
+			const startX = e.pageX;
+			const startWidth = th.offsetWidth;
+
+			const handleMouseMove = (e) => {
+				const newWidth = startWidth + (e.pageX - startX);
+				table.style.setProperty(`--width-col-${colIndex}`, `${newWidth}px`);
+				table.style.setProperty(`--text-wrap-col-${colIndex}`, "nowrap");
+			};
+
+			const handleMouseUp = () => {
+				document.removeEventListener("mousemove", handleMouseMove);
+				document.removeEventListener("mouseup", handleMouseUp);
+			};
+
+			document.addEventListener("mousemove", handleMouseMove);
+			document.addEventListener("mouseup", handleMouseUp);
+		});
+	});
 }
--- public/fileProcessing/fileProcessingCortos.js
+++ public/fileProcessing/fileProcessingCortos.js
@@ -1,275 +1,273 @@
 // fileProcessing.js
-import { insertarThead, mostrarNombreArchivo, getHeaderPosition } from '../JS/operations.js';
-import { getSelectedValueFromURL, insertarPageBreak } from '../JS/funcionesGlobales.js';
-import { createFiltersCheckbox } from '../JS/checkBox.js';
-import { sortValueNumeric, ordenarPorBodega, ordenarPorPiso } from '../JS/sortTable.js';
-import { insertarPageBreakPorPiso } from './cortos/ordenarPorPiso.js';
+import { insertarThead, mostrarNombreArchivo, getHeaderPosition, setRezizeEvent } from "../JS/operations.js";
+import { getSelectedValueFromURL, insertarPageBreak } from "../JS/funcionesGlobales.js";
+import { createFiltersCheckbox } from "../JS/checkBox.js";
+import { sortValueNumeric, ordenarPorBodega, ordenarPorPiso } from "../JS/sortTable.js";
+import { insertarPageBreakPorPiso } from "./cortos/ordenarPorPiso.js";
 
-let dataTable = '';
+let dataTable = "";
 
 async function procesarArchivo(file) {
-  console.log('[Procesar Archivo]');
-  const loadingContainer = document.getElementById('loading-container');
+	console.log("[Procesar Archivo]");
+	const loadingContainer = document.getElementById("loading-container");
 
-  // Mostrar la animación de carga
-  mostrarAnimacionDeCarga(loadingContainer);
+	// Mostrar la animación de carga
+	mostrarAnimacionDeCarga(loadingContainer);
 
-  try {
-    const data = await file.arrayBuffer();
+	try {
+		const data = await file.arrayBuffer();
 
-    // Parse and load first worksheet
-    const wb = XLSX.read(data);
-    const ws = wb.Sheets[wb.SheetNames[0]];
+		// Parse and load first worksheet
+		const wb = XLSX.read(data);
+		const ws = wb.Sheets[wb.SheetNames[0]];
 
-    // Create HTML table
-    const html = XLSX.utils.sheet_to_html(ws);
-    tablePreview.innerHTML = html;
-    dataTable = html;
+		// Create HTML table
+		const html = XLSX.utils.sheet_to_html(ws);
+		tablePreview.innerHTML = html;
+		dataTable = html;
 
-    // Mostrar la tabla y ocultar la animación de carga
-    ocultarAnimacionDeCarga(loadingContainer);
+		// Mostrar la tabla y ocultar la animación de carga
+		ocultarAnimacionDeCarga(loadingContainer);
 
-    modifyTable();
-  } catch (error) {
-    console.error('Error al procesar el archivo:', error);
-    // Ocultar la animación de carga en caso de error
-    ocultarAnimacionDeCarga(loadingContainer);
-  }
+		modifyTable();
+	} catch (error) {
+		console.error("Error al procesar el archivo:", error);
+		// Ocultar la animación de carga en caso de error
+		ocultarAnimacionDeCarga(loadingContainer);
+	}
 }
 
 export function handleFile(file, e, fileInput) {
-  const form = document.querySelector('.container-file-upload-form > form');
-  if (file) {
-    const reader = new FileReader();
+	const form = document.querySelector(".container-file-upload-form > form");
+	if (file) {
+		const reader = new FileReader();
 
-    reader.onload = function (e) {
-      const data = e.target.result;
+		reader.onload = function (e) {
+			const data = e.target.result;
 
-      if (!data) return;
+			if (!data) return;
 
-      // Verificar la extensión del archivo para determinar el tipo de parseo
-      const extension = file.name.split('.').pop().toLowerCase();
+			// Verificar la extensión del archivo para determinar el tipo de parseo
+			const extension = file.name.split(".").pop().toLowerCase();
 
-      if (extension === 'xlsx' || extension === 'csv' || extension === 'xls') {
-        procesarArchivo(file);
-        mostrarNombreArchivo(fileInput);
-        form && form.reset();
-      } else {
-        console.log('Formato de archivo no compatible');
-        alert('Formato de archivo no compatible');
-      }
-    };
+			if (extension === "xlsx" || extension === "csv" || extension === "xls") {
+				procesarArchivo(file);
+				mostrarNombreArchivo(fileInput);
+				form && form.reset();
+			} else {
+				console.log("Formato de archivo no compatible");
+				alert("Formato de archivo no compatible");
+			}
+		};
 
-    reader.readAsArrayBuffer(file);
-  }
+		reader.readAsArrayBuffer(file);
+	}
 }
 
 function modifyTable() {
-  console.log('[Modify Table]');
-  insertarThead()
-    .then(() => {
-      const valorDeLaURL = getSelectedValueFromURL('ordenar') ?? null;
-      if (valorDeLaURL && valorDeLaURL !== 'NoOrdenar') {
-        ordenarTabla()
-          .then(result => {
-            const { header, position } = result;
+	console.log("[Modify Table]");
+	insertarThead()
+		.then(() => {
+			const valorDeLaURL = getSelectedValueFromURL("ordenar") ?? null;
+			if (valorDeLaURL && valorDeLaURL !== "NoOrdenar") {
+				ordenarTabla()
+					.then((result) => {
+						const { header, position } = result;
 
-            if (header.toLowerCase().trim() === 'erp order' && position) {
-              let showColumns = [2, 3, 4, 6, 8];
-              showColumns = showColumns.map(value => value - 1);
+						if (header.toLowerCase().trim() === "erp order" && position) {
+							let showColumns = [2, 3, 4, 6, 8];
+							showColumns = showColumns.map((value) => value - 1);
 
-              insertarPageBreak(position)
-                .then()
-                .catch(err => {
-                  console.error('Error al insertar el salto de página:', err);
-                });
+							insertarPageBreak(position)
+								.then()
+								.catch((err) => {
+									console.error("Error al insertar el salto de página:", err);
+								});
 
-              tranformarTotalQty();
-              createFiltersCheckbox(showColumns, true);
-            } else if (header.toLowerCase().trim() === 'pedido' && position) {
-              let showColumns = [1, 2, 3, 4, 5];
-              showColumns = showColumns.map(value => value - 1);
+							tranformarTotalQty();
+							createFiltersCheckbox(showColumns, true);
+						} else if (header.toLowerCase().trim() === "pedido" && position) {
+							let showColumns = [1, 2, 3, 4, 5];
+							showColumns = showColumns.map((value) => value - 1);
 
-              insertarPageBreak(position)
-                .then(value => console.log(value))
-                .catch(err => {
-                  console.error('Error al insertar el salto de página:', err);
-                });
+							insertarPageBreak(position)
+								.then((value) => console.log(value))
+								.catch((err) => {
+									console.error("Error al insertar el salto de página:", err);
+								});
 
-              tranformarTotalQty();
-              createFiltersCheckbox(showColumns, true);
-            } else if (header.toLowerCase().trim() === 'bodega' && position) {
-              insertarPageBreak(position)
-                .then(value => console.log(value))
-                .catch(err => {
-                  console.error('Error al insertar el salto de página:', err);
-                });
+							tranformarTotalQty();
+							createFiltersCheckbox(showColumns, true);
+						} else if (header.toLowerCase().trim() === "bodega" && position) {
+							insertarPageBreak(position)
+								.then((value) => console.log(value))
+								.catch((err) => {
+									console.error("Error al insertar el salto de página:", err);
+								});
 
-              createFiltersCheckbox();
-            } else if (header.toLowerCase().trim() === 'por piso' && position) {
-              console.log('[Ordenar pos piso]');
-              insertarPageBreakPorPiso(position)
-                .then(value => console.log(value))
-                .catch(err => {
-                  console.error('Error al insertar el salto de página:', err);
-                });
+							createFiltersCheckbox();
+						} else if (header.toLowerCase().trim() === "por piso" && position) {
+							console.log("[Ordenar pos piso]");
+							insertarPageBreakPorPiso(position)
+								.then((value) => console.log(value))
+								.catch((err) => {
+									console.error("Error al insertar el salto de página:", err);
+								});
 
-              createFiltersCheckbox();
-            } else {
-              createFiltersCheckbox();
-            }
-          })
-          .catch(err => {
-            console.error('Error al ordenar la Tabla:', err);
-            createFiltersCheckbox();
-          });
-      } else {
-        createFiltersCheckbox();
-      }
+							createFiltersCheckbox();
+						} else {
+							createFiltersCheckbox();
+						}
 
-      eventoDeOrdenarPorParametro()
-        .then(value => console.log(value))
-        .catch(err => {
-          console.error('Error al crear el evento Ordenar Por Parametro:', err);
-        });
-    })
-    .catch(err => {
-      console.error('Error al insetar el Thead:', err);
-    });
+						setRezizeEvent();
+					})
+					.catch((err) => {
+						console.error("Error al ordenar la Tabla:", err);
+						createFiltersCheckbox();
+					});
+			} else {
+				createFiltersCheckbox();
+			}
+
+			eventoDeOrdenarPorParametro()
+				.then((value) => console.log(value))
+				.catch((err) => {
+					console.error("Error al crear el evento Ordenar Por Parametro:", err);
+				});
+		})
+		.catch((err) => {
+			console.error("Error al insetar el Thead:", err);
+		});
 }
 
 function ordenarTabla() {
-  console.log('[Ordenar tabla]');
-  return new Promise((resolve, reject) => {
-    const valorDeLaURL = getSelectedValueFromURL('ordenar') ?? null;
-    const table = document.querySelector('#tablePreview table');
+	console.log("[Ordenar tabla]");
+	return new Promise((resolve, reject) => {
+		const valorDeLaURL = getSelectedValueFromURL("ordenar") ?? null;
+		const table = document.querySelector("#tablePreview table");
 
-    if (!table) {
-      return reject('No se encontro la tabla con el id: #tablePreview');
-    }
+		if (!table) {
+			return reject("No se encontro la tabla con el id: #tablePreview");
+		}
 
-    if (!valorDeLaURL) {
-      return reject('No se encontro el valor del parametro de la URL [ordenar]');
-    }
+		if (!valorDeLaURL) {
+			return reject("No se encontro el valor del parametro de la URL [ordenar]");
+		}
 
-    const rows = Array.from(table.querySelectorAll('tbody tr'));
-    const headerRow = table.rows[0]; // Obtener la primera fila (encabezados)
+		const rows = Array.from(table.querySelectorAll("tbody tr"));
+		const headerRow = table.rows[0]; // Obtener la primera fila (encabezados)
 
-    let headerPositionElement = null;
+		let headerPositionElement = null;
 
-    if (valorDeLaURL.toLowerCase().trim() === 'erp order') {
-      headerPositionElement = getHeaderPosition(headerRow.cells, [valorDeLaURL]);
+		if (valorDeLaURL.toLowerCase().trim() === "erp order") {
+			headerPositionElement = getHeaderPosition(headerRow.cells, [valorDeLaURL]);
 
-      if (headerPositionElement) {
-        sortValueNumeric(rows, table, headerPositionElement)
-          .then(value => console.log(value))
-          .catch(err => {
-            console.error('Error al ordenar tabla:', err);
-          });
-      }
-    } else if (valorDeLaURL.toLowerCase().trim() === 'bodega') {
-      headerPositionElement = getHeaderPosition(headerRow.cells, ['bodega', 'zona', 'work_zone']);
+			if (headerPositionElement) {
+				sortValueNumeric(rows, table, headerPositionElement)
+					.then((value) => console.log(value))
+					.catch((err) => {
+						console.error("Error al ordenar tabla:", err);
+					});
+			}
+		} else if (valorDeLaURL.toLowerCase().trim() === "bodega") {
+			headerPositionElement = getHeaderPosition(headerRow.cells, ["bodega", "zona", "work_zone"]);
 
-      if (headerPositionElement) {
-        ordenarPorBodega(rows, table, headerPositionElement)
-          .then(value => console.log(value))
-          .catch(err => {
-            console.error('Error al ordenar tabla:', err);
-          });
-      }
-    } else if (valorDeLaURL.toLowerCase().trim() === 'pedido') {
-      headerPositionElement = getHeaderPosition(headerRow.cells, [
-        'pedido',
-        'id del pedido',
-        'shipment id',
-      ]);
+			if (headerPositionElement) {
+				ordenarPorBodega(rows, table, headerPositionElement)
+					.then((value) => console.log(value))
+					.catch((err) => {
+						console.error("Error al ordenar tabla:", err);
+					});
+			}
+		} else if (valorDeLaURL.toLowerCase().trim() === "pedido") {
+			headerPositionElement = getHeaderPosition(headerRow.cells, ["pedido", "id del pedido", "shipment id"]);
 
-      if (headerPositionElement) {
-        sortValueNumeric(rows, table, headerPositionElement)
-          .then(value => console.log(value))
-          .catch(err => {
-            console.error('Error al ordenar tabla:', err);
-          });
-      }
-    } else if (valorDeLaURL.toLowerCase().trim() === 'por piso') {
-      headerPositionElement = getHeaderPosition(headerRow.cells, ['bodega', 'zona', 'work_zone']);
+			if (headerPositionElement) {
+				sortValueNumeric(rows, table, headerPositionElement)
+					.then((value) => console.log(value))
+					.catch((err) => {
+						console.error("Error al ordenar tabla:", err);
+					});
+			}
+		} else if (valorDeLaURL.toLowerCase().trim() === "por piso") {
+			headerPositionElement = getHeaderPosition(headerRow.cells, ["bodega", "zona", "work_zone"]);
 
-      if (headerPositionElement) {
-        ordenarPorPiso(rows, table, headerPositionElement)
-          .then(value => console.log(value))
-          .catch(err => {
-            console.error('Error al ordenar tabla:', err);
-          });
-      }
-    }
+			if (headerPositionElement) {
+				ordenarPorPiso(rows, table, headerPositionElement)
+					.then((value) => console.log(value))
+					.catch((err) => {
+						console.error("Error al ordenar tabla:", err);
+					});
+			}
+		}
 
-    resolve({ header: valorDeLaURL, position: headerPositionElement });
-  });
+		resolve({ header: valorDeLaURL, position: headerPositionElement });
+	});
 }
 
 function tranformarTotalQty() {
-  const valorDeLaURL = getSelectedValueFromURL('ordenar');
+	const valorDeLaURL = getSelectedValueFromURL("ordenar");
 
-  if (!valorDeLaURL) return;
+	if (!valorDeLaURL) return;
 
-  if (!valorDeLaURL === '8') return;
+	if (!valorDeLaURL === "8") return;
 
-  const filas = tablePreview.querySelectorAll('tr');
+	const filas = tablePreview.querySelectorAll("tr");
 
-  if (!filas) return;
+	if (!filas) return;
 
-  const totalQty = filas[0].querySelector('[data-v="Total qty"]');
+	const totalQty = filas[0].querySelector('[data-v="Total qty"]');
 
-  if (!totalQty) return;
+	if (!totalQty) return;
 
-  // Iterar sobre las filas
-  filas.forEach((fila, index) => {
-    // Ignorar la primera fila (encabezados)
-    if (index === 0) return;
-    const tdTotalQty = fila.querySelector('td:nth-child(6)');
+	// Iterar sobre las filas
+	filas.forEach((fila, index) => {
+		// Ignorar la primera fila (encabezados)
+		if (index === 0) return;
+		const tdTotalQty = fila.querySelector("td:nth-child(6)");
 
-    if (totalQty) {
-      const value = tdTotalQty.innerHTML;
+		if (totalQty) {
+			const value = tdTotalQty.innerHTML;
 
-      // Verificar el valor es numérico
-      const valueNumeric = !isNaN(parseFloat(value)) && isFinite(value);
-      if (valueNumeric) {
-        tdTotalQty.innerHTML = parseFloat(value);
-      }
-    }
-  });
+			// Verificar el valor es numérico
+			const valueNumeric = !isNaN(parseFloat(value)) && isFinite(value);
+			if (valueNumeric) {
+				tdTotalQty.innerHTML = parseFloat(value);
+			}
+		}
+	});
 }
 
 function eventoDeOrdenarPorParametro() {
-  return new Promise((resolve, reject) => {
-    const loadingContainer = document.getElementById('loading-container');
+	return new Promise((resolve, reject) => {
+		const loadingContainer = document.getElementById("loading-container");
 
-    if (!loadingContainer) {
-      return reject('Error loadingContainer no existe');
-    }
+		if (!loadingContainer) {
+			return reject("Error loadingContainer no existe");
+		}
 
-    // Event listener para los cambios en los inputs
-    document.querySelectorAll('.filters input[name="ordernar"]').forEach(input => {
-      input.addEventListener('change', function () {
-        const selectedValue = this.value;
+		// Event listener para los cambios en los inputs
+		document.querySelectorAll('.filters input[name="ordernar"]').forEach((input) => {
+			input.addEventListener("change", function () {
+				const selectedValue = this.value;
 
-        mostrarAnimacionDeCarga(loadingContainer);
-        tablePreview.innerHTML = dataTable;
-        modifyTable();
-        ocultarAnimacionDeCarga(loadingContainer);
-      });
-    });
+				mostrarAnimacionDeCarga(loadingContainer);
+				tablePreview.innerHTML = dataTable;
+				modifyTable();
+				ocultarAnimacionDeCarga(loadingContainer);
+			});
+		});
 
-    resolve('Evento para modificar la tabla  por parametro Creado con exito');
-  });
+		resolve("Evento para modificar la tabla  por parametro Creado con exito");
+	});
 }
 
 function mostrarAnimacionDeCarga(loadingContainer) {
-  loadingContainer.style.display = 'flex';
-  tablePreview.innerHTML = '';
+	loadingContainer.style.display = "flex";
+	tablePreview.innerHTML = "";
 }
 
 function ocultarAnimacionDeCarga(loadingContainer) {
-  tablePreview.style.display = 'block';
-  loadingContainer.style.display = 'none';
+	tablePreview.style.display = "block";
+	loadingContainer.style.display = "none";
 }
